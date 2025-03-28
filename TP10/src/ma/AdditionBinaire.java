7015ffab508dd6c352eba0a9dbec62e2f7b406ca
imane elkourari <imanekourari@gmail.com>
2025-03-28 23:49:44 +0100

    Update AdditionBinaire.java

--- TP10/src/ma/AdditionBinaire.java
+++ TP10/src/ma/AdditionBinaire.java
@@ -1,35 +1,31 @@
 package ma;
 
+import java.io.*;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class AdditionBinaire {
-    public static String additionBinaire(String a, String b) {
-        StringBuilder resultat = new StringBuilder();
-        int carry = 0;
-        int i = a.length() - 1;
-        int j = b.length() - 1;
-
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int somme = carry;
-            if (i >= 0) somme += a.charAt(i--) - '0';
-            if (j >= 0) somme += b.charAt(j--) - '0';
-            resultat.append(somme % 2);
-            carry = somme / 2;
-        }
-
-        return resultat.reverse().toString();
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws FileNotFoundException {
+       
+        Scanner scanner = new Scanner(new File("bin.in"));
+        PrintWriter output = new PrintWriter(new File("bin.out"));
+        
         int N = scanner.nextInt();
-        scanner.nextLine(); // Pour ignorer le saut de ligne
-
+        scanner.nextLine(); 
+        
         for (int i = 1; i <= N; i++) {
-            String a = scanner.nextLine();
-            String b = scanner.nextLine();
-            String resultat = additionBinaire(a, b);
-            System.out.println(i + " " + resultat);
+            String line = scanner.nextLine();
+            String[] numbers = line.split(" ");
+            
+            BigInteger a = new BigInteger(numbers[0], 2);
+            BigInteger b = new BigInteger(numbers[1], 2);
+            BigInteger sum = a.add(b);
+            
+            output.println(i + " " + sum.toString(2));
         }
+        
+        output.flush();
+        output.close();
+        scanner.close();
     }
 }
