4920ee84aed9ca6596a8247f828db9ceea86e49c
imane elkourari <imanekourari@gmail.com>
2025-03-29 01:00:08 +0100

    Update NombresOpposes.java

--- TP10/src/ma/NombresOpposes.java
+++ TP10/src/ma/NombresOpposes.java
@@ -7,32 +7,19 @@ import java.util.Set;
 public class NombresOpposes {
 
     public static void main(String[] args) throws IOException {
-        // Création d'un BufferedReader pour lire le fichier
         BufferedReader br = new BufferedReader(new FileReader("System.in"));
-        
-        // Lire la taille du tableau
         int n = Integer.parseInt(br.readLine().trim());
-
-        // Créer un ensemble pour stocker les entiers du tableau
         Set<Integer> nombres = new HashSet<>();
-        
-        // Lire la deuxième ligne contenant les entiers
         String[] arr = br.readLine().split(" ");
-        
-        // Ajouter les entiers dans l'ensemble
         for (String s : arr) {
             nombres.add(Integer.parseInt(s));
         }
-
-        // Compter les entiers distincts positifs ayant leur opposé dans le tableau
         int count = 0;
         for (int num : nombres) {
             if (num > 0 && nombres.contains(-num)) {
                 count++;
             }
         }
-
-        // Afficher le résultat dans un fichier
         PrintWriter writer = new PrintWriter(new FileWriter("System.out"));
         writer.println(count);
         writer.close();
